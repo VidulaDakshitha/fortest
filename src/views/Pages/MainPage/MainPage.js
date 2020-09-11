c95aa91e558fef91062f7b58ad10f39307a55a0b
Sachini <sachini.witha@gmail.com>
2020-09-11 14:28:23 +0530

    commit2

--- src/views/Pages/MainPage/MainPage.js
+++ src/views/Pages/MainPage/MainPage.js
@@ -172,7 +172,7 @@ class MainPage extends Component{
 <div className="card" style={{width: "18rem"}}>
   <img className="card-img-top" src={img2} alt="Card image cap"></img>
   <div className="card-body">
-    <h5 className="card-title">Join As Merchant</h5>
+    <h5 className="card-title">Dinal fernando</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <a  className="btn btn-success" href="/#/register" style={{color:"white"}} >Proceed to register</a>
   </div>
